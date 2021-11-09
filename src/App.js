f8a6a0db760d35c082cd8975895b9411ddaa1181
PH Saurav <phsaurav29@gmail.com>
2021-11-10 03:24:59 +0600

    React Router Added 🗺

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
+import Home from './containers/Home/Home';
+import NotFound from './containers/NotFound/NotFound';
 
 function App() {
 	return (
 		<div>
-			<h1 className="text-center text-5xl">App is running</h1>
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<Redirect to="/home"></Redirect>
+					</Route>
+					<Route path="/home">
+						<Home></Home>
+					</Route>
+					<Route>
+						<NotFound></NotFound>
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
