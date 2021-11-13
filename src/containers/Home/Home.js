5fa787ffab61d929f8b1a176e73dc4b003b4cfa2
PH Saurav <phsaurav29@gmail.com>
2021-11-13 19:20:32 +0600

    Link Fix 🔗

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import Hero from '../../components/Hero/Hero';
